389e88784af3b129207cf11532e6a3bdf19d5000
yiyu66 <1226572422@qq.com>
2021-11-16 23:51:26 +0800

    路由添加测试

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import "./App.css";
+// import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// 导入组件
 import Home from "./pages/Home";
+import CityList from "./pages/CityList";
 // import logo from './logo.svg';
 function App() {
   return (
-    <div className="App">
-      项目根组件
-      <Home></Home>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <Router>
+      <div className="App">
+        项目根组件
+        {/* 配置导航菜单 */}
+        <link to="/home">首页</link>
+        <link to="/cityList">城市选择</link>
+        {/* 配置路由 */}
+        <Route path="/home" component={Home}></Route>
+        <Route path="/cityList" component={CityList}></Route>
+        {/* <Home></Home>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/CityList/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CityList extends Component {
+  render() {
+    return <div>城市选择页面</div>;
+  }
+}
