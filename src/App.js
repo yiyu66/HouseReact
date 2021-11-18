76398d955b7ab70bb5bc895a36c04ca12c17ee4d
yiyu66 <1226572422@qq.com>
2021-11-18 17:27:35 +0800

    配置react-router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import "./App.css";
+// import Home from "./pages/Home"
+// import { render } from "react-dom";
+import { Outlet, Link } from "react-router-dom";
 // import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// 导入组件
-import Home from "./pages/Home";
-import CityList from "./pages/CityList";
+// import CityList from "./pages/CityList";
+
 // import logo from './logo.svg';
 function App() {
   return (
-    <Router>
-      <div className="App">
-        项目根组件-test
-        {/* 配置导航菜单 */}
-        <link to="/home">首页</link>
-        <link to="/cityList">城市选择</link>
-        {/* 配置路由 */}
-        <Route path="/home" component={Home}></Route>
-        <Route path="/cityList" component={CityList}></Route>
-        {/* <Home></Home>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-      </div>
-    </Router>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Expenses from "./pages/CityList/expenses";
+import Invoices from "./pages/CityList/invoices";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
