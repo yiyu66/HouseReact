3af734fec80b6be324c41b59d221da632b05a798
yiyu66 <1226572422@qq.com>
2021-11-21 23:22:09 +0800

    HomeHeader;less准备

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from './router/AppRouter'
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
